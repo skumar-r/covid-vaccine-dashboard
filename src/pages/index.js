0f7d82700f4182ba4cb5abb6de812d43c63084b4
sindhukumar <sindhu.kumar.r@gmail.com>
2021-05-26 13:39:41 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,94 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from 'react';
 import { StaticImage } from "gatsby-plugin-image"
-
+import axios from 'axios';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import DatePicker from 'react-date-picker';
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const columns = [{
+  dataField: 'center_id',
+  text: 'Centre ID'
+}, {
+  dataField: 'name',
+  text: 'Address',
+  formatter: (cell, row, rowIndex, extraData) => (
+    <div>
+      <span>Name: {row.name}</span>
+      <br />
+      <span>Address: {row.address}</span>
+      <br />
+      <span>Block Name: {row.block_name}</span>
+      <br />
+      <span>Pin Code: {row.pincode}</span>
+    </div>
+  )
+},
+{
+  dataField: 'available_capacity',
+  text: 'Available Capacity'
+},
+{
+  dataField: 'vaccine',
+  text: 'Vaccine'
+},
+{
+  dataField: 'min_age_limit',
+  text: 'Age Limit'
+},
+{
+  dataField: 'slots',
+  text: 'Slots Available',
+  formatter: (cell, row, rowIndex, extraData) => (
+    <div>{row.slots.join('\n')}</div>    
+  )
+},
+{
+  dataField: 'fee_type',
+  text: 'Free/Paid'
+}
+];
+
+const IndexPage = () => {
+  const [value, setValue] = useState(new Date());
+  const [data, setData] = useState({ sessions: [] });
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      let [month, date, year]    = new Date(value).toLocaleDateString("en-US").split("/");
+      const result = await axios(
+        'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=544&date='+(date+'-'+month+'-'+year)
+      );
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, [value]);
+
+
+  return (
+    <Layout>
+      <Seo title="Dashboard" />         
+      <StaticImage
+        src="../images/covid-19-anim.gif"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Covid-19 India"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>You can check the vaccine availability in Kanyakumari district.</p>
+      <p>Select a Date: <DatePicker
+        onChange={setValue}
+        value={value}
+      />
+      </p>
+      <p>
+        <BootstrapTable keyField='centre_id' data={data.sessions} columns={columns} />
+      </p>
+    </Layout>)
+
+}
 
 export default IndexPage
